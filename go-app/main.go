137fa7fd439be875ec91ba0e01f9214d2ad39a8a
Akash <akashsingh2210670@gmail.com>
2025-02-22 19:06:32 +0530

    refactor: remove unused files and code for cleanup
    Signed-off-by: Akash <akashsingh2210670@gmail.com>

--- go-app/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/keploy/gin-redis/server"
-)
-
-func main() {
-	server.Init()
-}
--- go-app/structure/request/verifyOTP.go
+++
@@ -1,6 +0,0 @@
-package requestStruct
-
-type OTPRequest struct {
-	OTP   int    `json:"otp" binding:"required"`
-	Email string `json:"email" binding:"required"`
-}
