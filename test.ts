435af6a5609b8272699f78411de936517fd806ac
slashexx <dhruvpuri.35@gmail.com>
2025-02-23 01:34:51 +0530

    okay
    Signed-off-by: slashexx <dhruvpuri.35@gmail.com>

--- test.ts
+++ test.ts
@@ -1,3 +1,3 @@
 console.log("This is a test PR and the following command prints hello world.")
-// fix : the following line
-console.log("World hello.")
+// fix : need to comment out the following line
+// console.log("World hello.")
