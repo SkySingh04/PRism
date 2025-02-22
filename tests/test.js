097051023b923e8cb3a57fdd8ccc7d86a512d3c6
Akash <akashsingh2210670@gmail.com>
2025-02-22 23:29:21 +0530

    feat: implement basic math operations and corresponding tests
    Signed-off-by: Akash <akashsingh2210670@gmail.com>

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+describe('Math operations', () => {
+    test('add should return the sum', () => {
+        expect(add(2, 3)).toBe(5);
+    });
+
+    test('subtract should return the difference', () => {
+        expect(subtract(5, 3)).toBe(2);
+    });
+});
