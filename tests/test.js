2b7719d16a19866642deed691a6e668169464f59
Akash <akashsingh2210670@gmail.com>
2025-02-22 23:27:02 +0530

    refactor: remove unused math functions and their corresponding tests
    Signed-off-by: Akash <akashsingh2210670@gmail.com>

--- tests/test.js
+++
@@ -1,9 +0,0 @@
-describe('Math operations', () => {
-    test('add should return the sum', () => {
-        expect(add(2, 3)).toBe(5);
-    });
-
-    test('subtract should return the difference', () => {
-        expect(subtract(5, 3)).toBe(2);
-    });
-});
