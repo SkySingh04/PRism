d39e98813b37d2e1303b2a05c807ab1b324efb61
Akash <akashsingh2210670@gmail.com>
2025-02-23 01:09:43 +0530

    refactor: update handleSecurityWorkflowTrigger to use 'main' as the workflow dispatch reference
    Signed-off-by: Akash <akashsingh2210670@gmail.com>

--- security.ts
+++ security.ts
@@ -1,6 +1,6 @@
 export async function handleSecurityWorkflowTrigger(context: any) {
     const { owner, repo } = context.repo();
-    const { base } = context.payload.pull_request;
+    // const { base } = context.payload.pull_request;
   
     try {
       await context.octokit.issues.createComment({
@@ -10,7 +10,7 @@ export async function handleSecurityWorkflowTrigger(context: any) {
       });
       
       await context.octokit.actions.createWorkflowDispatch({
-        owner, repo, workflow_id: 'security.yaml', ref : base.ref
+        owner, repo, workflow_id: 'security.yaml', ref : "main"
       });
     } catch (error : any) {
       if (error.status === 404) {
